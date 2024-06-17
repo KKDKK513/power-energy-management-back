dff651b52345f1b3add2ba641fc81c7b67e7368e
userName <userEmail>
2024-06-17 08:37:35 +0800

    wuke

--- routes/getData/task1.js
+++ routes/getData/task1.js
@@ -86,25 +86,15 @@ const canIds = [
     0x1C300010, 0x1C301010, 0x1C302010, 0x1C303010, 0x1C304010, 0x1C305010, 0x1C306010, 0x1C307010, 0x1C308010, 0x1C309010, 0x1C30A010, 0x1C30B010, 0x1C30C010, 0x1C30D010, 0x1C30E010, 0x1C30F010, 0x1C310010, 0x1C311010, 0x1C312010, 0x1C313010, 0x1C314010, 0x1C315010, 0x1C316010, 0x1C317010, 0x1C318010, 0x1C319010, 0x1C31A010, 0x1C31B010, 0x1C31C010, 0x1C31D010, 0x1C31E010, 0x1C31F010, 0x1C320010, 0x1C321010, 0x1C322010, 0x1C323010, 0x1C324010, 0x1C325010, 0x1C326010, 0x1C327010, 0x1C328010, 0x1C329010, 0x1C32A010, 0x1C32B010, 0x1C32C010, 0x1C32D010, 0x1C32E010, 0x1C32F010, 0x1C330010, 0x1C331010, 0x1C332010, 0x1C333010, 0x1C334010, 0x1C335010, 0x1C336010, 0x1C337010, 0x1C338010, 0x1C339010, 0x1C33A010, 0x1C33B010,
     0x1C402010, 0x1C403010, 0x1C404010, 0x1C407010, 0x1C408010, 0x1C409010, 0x1C40A010, 0x1C40B010,
     0x1C40E010, 0x1C40F010, 0x1C412010, 0x1C413010, 0x1C414010, 0x1C417010, 0x1C418010, 0x1C419010,
-    0x1C100010, 0x1C101010, 0x1C102010, 0x1C103010, 0x1C104010, 0x1C105010, 0x1C106010, 0x1C107010, 0x1C108010, 0x1C109010, 0x1C10A010, 0x1C10B010, 0x1C10C010, 0x1C10D010, 0x1C10E010, 0x1C10F010, 0x1C110010, 0x1C111010, 0x1C112010, 0x1C113010, 0x1C114010, 0x1C115010, // 可读
-    0x1C116010, 0x1C117010, 0x1C118010, 0x1C119010, 0x1C11A010, 0x1C11B010, 0x1C11C010, 0x1C11D010, 0x1C11E010, 0x1C11F010, 0x1C120010, 0x1C121010, 0x1C122010, 0x1C123010, 0x1C124010, 0x1C125010, 0x1C126010, 0x1C127010, 0x1C128010, 0x1C129010, 0x1C12A010, 0x1C12B010, // 可读
-    0x1C12C010, 0x1C12D010, 0x1C12E010, 0x1C12F010, 0x1C130010, 0x1C131010, 0x1C132010, 0x1C133010, 0x1C134010, 0x1C135010, 0x1C136010, 0x1C137010, 0x1C138010, 0x1C139010, 0x1C13A010, 0x1C13B010, 0x1C200010, 0x1C201010, 0x1C202010, 0x1C203010, 0x1C204010, 0x1C205010, // 可读
-    0x1C206010, 0x1C207010, 0x1C208010, 0x1C209010, 0x1C20A010, 0x1C20B010, 0x1C20C010, 0x1C20D010, 0x1C20E010, 0x1C20F010, 0x1C210010, 0x1C211010, 0x1C212010, 0x1C213010, 0x1C214010, 0x1C215010, // 可读
-    0x1C216010, 0x1C217010, 0x1C218010, 0x1C219010, 0x1C21A010, 0x1C21B010, 0x1C21C010, 0x1C21D010, 0x1C21E010, 0x1C21F010, 0x1C220010, 0x1C221010, 0x1C222010, 0x1C223010, 0x1C224010, 0x1C225010, 0x1C226010, 0x1C227010, 0x1C228010, 0x1C229010, 0x1C22A010, 0x1C22B010, // 可读
-    0x1C22C010, 0x1C22D010, 0x1C22E010, 0x1C22F010, 0x1C230010, 0x1C231010, 0x1C232010, 0x1C233010, 0x1C234010, 0x1C235010, 0x1C236010, 0x1C237010, 0x1C238010, 0x1C239010, 0x1C23A010, 0x1C23B010, // 可读
+    0x1C100010, 0x1C101010, 0x1C102010, 0x1C103010, 0x1C104010, 0x1C105010, 0x1C106010, 0x1C107010, 0x1C108010, 0x1C109010, 0x1C10A010, 0x1C10B010, 0x1C10C010, 0x1C10D010, 0x1C10E010, 0x1C10F010, 0x1C110010, 0x1C111010, 0x1C112010, 0x1C113010, 0x1C114010, 0x1C115010,
+    0x1C116010, 0x1C117010, 0x1C118010, 0x1C119010, 0x1C11A010, 0x1C11B010, 0x1C11C010, 0x1C11D010, 0x1C11E010, 0x1C11F010, 0x1C120010, 0x1C121010, 0x1C122010, 0x1C123010, 0x1C124010, 0x1C125010, 0x1C126010, 0x1C127010, 0x1C128010, 0x1C129010, 0x1C12A010, 0x1C12B010,
+    0x1C12C010, 0x1C12D010, 0x1C12E010, 0x1C12F010, 0x1C130010, 0x1C131010, 0x1C132010, 0x1C133010, 0x1C134010, 0x1C135010, 0x1C136010, 0x1C137010, 0x1C138010, 0x1C139010, 0x1C13A010, 0x1C13B010, 0x1C200010, 0x1C201010, 0x1C202010, 0x1C203010, 0x1C204010, 0x1C205010,
+    0x1C206010, 0x1C207010, 0x1C208010, 0x1C209010, 0x1C20A010, 0x1C20B010, 0x1C20C010, 0x1C20D010, 0x1C20E010, 0x1C20F010, 0x1C210010, 0x1C211010, 0x1C212010, 0x1C213010, 0x1C214010, 0x1C215010,
+    0x1C216010, 0x1C217010, 0x1C218010, 0x1C219010, 0x1C21A010, 0x1C21B010, 0x1C21C010, 0x1C21D010, 0x1C21E010, 0x1C21F010, 0x1C220010, 0x1C221010, 0x1C222010, 0x1C223010, 0x1C224010, 0x1C225010, 0x1C226010, 0x1C227010, 0x1C228010, 0x1C229010, 0x1C22A010, 0x1C22B010,
+    0x1C22C010, 0x1C22D010, 0x1C22E010, 0x1C22F010, 0x1C230010, 0x1C231010, 0x1C232010, 0x1C233010, 0x1C234010, 0x1C235010, 0x1C236010, 0x1C237010, 0x1C238010, 0x1C239010, 0x1C23A010, 0x1C23B010,
     0x1C800010, 0x1C804010, 0x1C808010, 0x1C809010, 0x1C82D010, 0x1C830010, 0x1C837010, 0x1C838010, 0x1C839010
 ];
-function checkAlarmStatus(alarmList) { // 检查故障状态
-    let alarmStatus = false;
-    for (let alarm of alarmList) {
-        if (alarm !== '0000000000000000') {
-            alarmStatus = true;
-            break;
-        }
-    }
-    return alarmStatus;
-}
-function checkAlarmStatus1(alarmList) { // 检查故障状态 pcs air
+function checkAlarmStatus1(alarmList) { 
     let alarmStatus = false;
     for (let alarm of alarmList) {
         if (alarm !== '0103020000b844') {
@@ -114,7 +104,7 @@ function checkAlarmStatus1(alarmList) { // 检查故障状态 pcs air
     }
     return alarmStatus;
 }
-function checkAlarmStatusBcu(alarmList) { // 检查故障状态 bcu
+function checkAlarmStatusBcu(alarmList) { 
     let alarmStatus = false;
     for (let alarm of alarmList) {
         if (alarm !== '0000000000000000') {
@@ -125,7 +115,6 @@ function checkAlarmStatusBcu(alarmList) { // 检查故障状态 bcu
     return alarmStatus;
 }
 async function readAllCanMessagesAtInterval(canIds) {
-    let alarmStatus;
     let currentData = [];
     let currentData405 = [];
     let currentData406 = [];
@@ -133,12 +122,11 @@ async function readAllCanMessagesAtInterval(canIds) {
     let currentData415 = [];
     let currentData401 = [];
     let currentData400 = [];
-    let currentAlarm = [];
     let currentData0x1C000010 = [];
     let currentData0x1C001010 = [];
     let currentData0x1C002010 = [];
     let currentData0x1C003010 = [];
-    let results = {};  // 创建一个空对象用于保存结果
+    let results = {};
     for (const canId of canIds) {
         const msg = await handleAndReadCanMessages("can0", canId);
         const hexData = msg.data.toString('hex');
@@ -173,7 +161,7 @@ async function readAllCanMessagesAtInterval(canIds) {
                 const decimalResult415 = parseInt(result, 16);
                 const res = decimalResult415.toString(2);
                 const resSecondLastBit = res[res.length - 2];
-                const formattedResult = resSecondLastBit === '1' ? '0x01' : '0x00'; // 可能会把单引号去掉 记录一下
+                const formattedResult = resSecondLastBit === '1' ? '0x01' : '0x00';
                 results['0x1C415010'] = formattedResult;
             } else if (canId === 0x1C401010) { // B+总压 总电压 - 完成
                 currentData401.push(parts.join(''));
@@ -187,7 +175,7 @@ async function readAllCanMessagesAtInterval(canIds) {
                 const result = str.substring(4, 8);
                 results['0x1C400010'] = result;
                 results['0x1C400010111'] = result0;
-            } else if (canId === 0x1C000010) { // 暂时读不出来
+            } else if (canId === 0x1C000010) {
                 currentData0x1C000010.push(parts.join(''));
             } else if (canId === 0x1C001010) {
                 currentData0x1C001010.push(parts.join(''));
@@ -198,8 +186,6 @@ async function readAllCanMessagesAtInterval(canIds) {
             } else {
                 currentData.push(parts.join(''));
                 currentData = [...currentData405, ...currentData406, ...currentData40D, ...currentData415, ...currentData401, ...currentData400, ...currentData0x1C000010, ...currentData0x1C001010, ...currentData0x1C002010, ...currentData0x1C003010, ...currentData];
-                currentAlarm = [...currentData0x1C000010, ...currentData0x1C001010, ...currentData0x1C002010, ...currentData0x1C003010]
-                alarmStatus = checkAlarmStatus(currentAlarm)
                 return {
                     results: results,
                     currentData: currentData
@@ -231,9 +217,9 @@ async function updateStatus() {
     _pcsAlarm = await handleAndReadPCSMessages(pcsAlarmList);
     _airAlarm = await handleAndReadAirMessages(airAlarmList);
     _bcuAlarm = await readAllCanMessagesAtIntervalBcu(bcuAlarmList);
-    pcsStatus = checkAlarmStatus1(_pcsAlarm); // false无故障
-    airStatus = checkAlarmStatus1(_airAlarm); // false无故障
-    bcuStatus = checkAlarmStatusBcu(_bcuAlarm); // false无故障
+    pcsStatus = checkAlarmStatus1(_pcsAlarm);
+    airStatus = checkAlarmStatus1(_airAlarm);
+    bcuStatus = checkAlarmStatusBcu(_bcuAlarm);
     allStatus = !(airStatus || pcsStatus || bcuStatus);
 }
 
@@ -256,7 +242,7 @@ async function readAllCan1MessagesAtInterval1() {
         let secondPart2 = '0x' + totalVoltage.substring(2);
         let thirdPart3 = '0x' + chargeDischargeCurrent.substring(0, 2);
         let fourPart4 = '0x' + chargeDischargeCurrent.substring(2);
-        const canData1 = [firstPart1, secondPart2, thirdPart3, fourPart4, 0x8F, 0xE4, 0x70, 0x1C]; // 8FE4对应-28700补码
+        const canData1 = [firstPart1, secondPart2, thirdPart3, fourPart4, 0x8F, 0xE4, 0x70, 0x1C];
         await handleAndReadCanMessagesNew("can1", 0x180250F1, canData1);
     } catch (error) {
         console.error('Error in readAllCan1MessagesAtInterval1:', error);
@@ -301,20 +287,20 @@ async function readAllCan1Messages() {
 
 async function startDataFetching() {
     try {
-        await updateStatus(); // 判断故障
+        await updateStatus();
         await delay(100);
-        await fetchData(); // 获取pcs can在bcu中需要的数据
+        await fetchData();
         await delay(100);
-        await readAllCan1Messages(); // pcs can
+        await readAllCan1Messages();
         await delay(100);
 
         hycc = setInterval(async () => {
             try {
-                await updateStatus(); // 判断故障
+                await updateStatus();
                 await delay(100);
-                await fetchData(); // 获取pcs can在bcu中需要的数据
+                await fetchData();
                 await delay(100);
-                await readAllCan1Messages(); // pcs can
+                await readAllCan1Messages();
                 await delay(100);
             } catch (error) {
                 console.error('Error in interval:', error);
@@ -325,7 +311,7 @@ async function startDataFetching() {
     }
 }
 
-function handleAndReadCanMessagesNew(channelName, canId, canData) { // 写
+function handleAndReadCanMessagesNew(channelName, canId, canData) {
     return new Promise(async (resolve, reject) => {
         const handler = handleCanMessages(channelName, canId);
         handler.send1(canData);
@@ -353,7 +339,7 @@ async function readAllCanMessagesAtInterval1(canIds) {
         const parts = hexData.match(/.{2}/g);
         const canIdhyc = decimalToHex(msg.id).substring(0, 5);
         const canId123 = decimalToHex(canId).substring(0, 5);
-        if (canId === 0x1C8E7010 || canId === 0x1C8E8010) { // 需要判断并簇/退簇
+        if (canId === 0x1C8E7010 || canId === 0x1C8E8010) { 
             const canIdOutIn = [0x1C414010]
             const _readMsg = await handleAndReadCanMessages('can0', canIdOutIn)
             const readMsg = _readMsg.data.toString('hex').match(/.{2}/g);
@@ -370,14 +356,14 @@ async function readAllCanMessagesAtInterval1(canIds) {
                             console.log('并簇成功');
                             return
                         } else {
-                            console.log('并簇失败 请重试'); // 可能有问题 是五一前最后一天下午加的 后续可能还需要加失败判断
+                            console.log('并簇失败 请重试');
                         }
                     } else if (canId === 0x1C8E8010) {
                         if (resultsTui === currentReadData[0]) {
                             console.log('退簇成功');
                             return
                         } else {
-                            console.log('退簇失败 请重试');  // 可能有问题 是五一前最后一天下午加的 后续可能还需要加失败判断
+                            console.log('退簇失败 请重试');
                         }
                     } else {
                         console.log('并簇退簇异常');
@@ -450,15 +436,15 @@ function isWriteStop() {
     }
     fs.writeFileSync(filePath, JSON.stringify(_data));
 }
-async function planA() {  // 待机-充电-关机
+async function planA() {  // 充电
     let chargeLevel = 0;
     try {
         await delay(1000)
-        await processData(commandB); // 并网
+        await processData(commandB);
         await delay(1000);
-        await processData(command_PowerOn);  // 开机
+        await processData(command_PowerOn);
         await delay(11000);
-        while (shouldContinuePlan === 1) { // 为了点停止可以让充电/放电停下来 shouldContinuePlan为2的时候会停下来 传2的时候会停止
+        while (shouldContinuePlan === 1) { 
             if (!allStatus) {
                 isWriteStop()
                 console.log('出现故障即将关机');
@@ -489,12 +475,12 @@ async function planA() {  // 待机-充电-关机
                         switch (chargeLevel) {
                             case 1:
                                 command_charging.data = -380;
-                                await processData(command_charging); // 更新充电功率
+                                await processData(command_charging);
                                 console.log(`电量未充满...正在充电中... 进行修改功率: ${command_charging.data}当前电压:`, currentDateTime, decimalResult);
                                 break;
                             case 2:
                                 command_charging.data = -190;
-                                await processData(command_charging); // 更新充电功率
+                                await processData(command_charging);
                                 console.log(`电量未充满...正在充电中... 进行修改功率: ${command_charging.data}当前电压:`, currentDateTime, decimalResult);
                                 break;
                             case 3:
@@ -505,7 +491,7 @@ async function planA() {  // 待机-充电-关机
                         if (chargeLevel >= 3) {
                             console.log('电量已充满', currentDateTime, decimalResult);
                             isWriteStop()
-                            break; // 达到三次后退出
+                            break;
                         }
                     } else {
                         console.log('电量未充满...正在充电中... 当前电压:', currentDateTime, decimalResult);
@@ -522,21 +508,21 @@ async function planA() {  // 待机-充电-关机
             }
         }
         console.log('@@@planA');
-        await processData(command_PowerDown); // 关机
+        await processData(command_PowerDown);
         await delay(2000);
     } catch (error) {
         console.error('处理数据时出现错误:', error);
         process.exit(1);
     }
 }
-async function planB() { // 待机-并网放电-关机
+async function planB() { // 放电
     try {
         await delay(1000);
-        await processData(commandB); // 并网
+        await processData(commandB);
         await delay(1000);
-        await processData(command_PowerOn);  // 开机
+        await processData(command_PowerOn);
         await delay(11000);
-        while (shouldContinuePlan === 1) { // 手动停
+        while (shouldContinuePlan === 1) {
             if (!allStatus) {
                 isWriteStop()
                 console.log('出现故障即将关机');
@@ -553,7 +539,7 @@ async function planB() { // 待机-并网放电-关机
                 startExecution()
                 break
             }
-            await processData(command_discharge); // 追加三级告警判断 依次修改command_discharge 放电
+            await processData(command_discharge);
             console.log('功率写入');
             try {
                 const _msg = await withTimeout(handleAndReadCanMessages('can0', [0x1C406010]), 5000, 'CAN message timeout');
@@ -568,7 +554,7 @@ async function planB() { // 待机-并网放电-关机
                     if (decimalResult <= 3005) {
                         await processData(command_PowerDown);
                         console.log('电量已放完', currentDateTime, decimalResult);
-                        await delay(5000) // 这里可以最后判断电表时间   await planA() 放电之后开始充电
+                        await delay(5000)
                         isWriteStop()
                         break;
                     } else {
@@ -586,7 +572,7 @@ async function planB() { // 待机-并网放电-关机
             }
         }
         console.log('@@@planB');
-        await processData(command_PowerDown); // 关机——>第三次放满电 / 暂停直接关机
+        await processData(command_PowerDown);
         await delay(2000);
     } catch (error) {
         console.error('处理数据时出现错误:', error);
@@ -604,10 +590,10 @@ async function startExecution() {
     console.log('111打印111');
     startTimewatch() 
     await readAllCanMessagesAtInterval1(canIdshyc)
-    await startDataFetching(); // 一直有
-    if (!allStatus) { // pcs开机之前如果有故障就直接return 因为还没有开机 所以不用再关机
+    await startDataFetching();
+    if (!allStatus) { 
         isWriteStop()
-        console.log('出现故障即将关机'); // 这里pcs压根没开机 直接return没毛病
+        console.log('出现故障即将关机'); 
         return
     }
 
@@ -618,7 +604,7 @@ async function startExecution() {
     let _isPlanB = targetTime.dischargeTime.filter(item => {
         return currentDateTimeNow >= item.split('-')[0] && currentDateTimeNow <= item.split('-')[1]
     })
-    if (_isPlanA.length > 0 || _isPlanB.length > 0) { // 判断时间到没到 时间到了就开始执行
+    if (_isPlanA.length > 0 || _isPlanB.length > 0) { 
         if (_isPlanA.length > 0) {
             executePlanA();
         }
@@ -626,9 +612,9 @@ async function startExecution() {
             executePlanB();
         }
         return
-    } else { // 如果时间没到 就开始走计时器直到时间到设定时间
+    } else { 
         hyc = setInterval(() => {
-            if (!allStatus) { // 出现故障 这里也没有开机 直接return即可
+            if (!allStatus) { 
                 isWriteStop()
                 console.log('出现故障即将关机');
                 return
@@ -641,7 +627,7 @@ async function startExecution() {
                 return currentDateTimeNow >= item.split('-')[0] && currentDateTimeNow <= item.split('-')[1]
             })
             if (_isPlanA.length > 0 || _isPlanB.length > 0) {
-                clearInterval(hyc) // 如果到了充电时间/放电时间 就把定时器清掉
+                clearInterval(hyc) 
                 if (_isPlanA.length > 0) {
                     executePlanA();
                 }
